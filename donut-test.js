7d3da598e6acf7f6be6856f15da7211463ffc2b6
Giordano <zagattigiordano@hotmail.it>
2023-10-18 12:44:22 +0200

    result page

--- donut-test.js
+++ donut-test.js
@@ -2,17 +2,18 @@ const container = document.getElementById("container");
 
 const segment = document.getElementsByClassName("segment")[0];
 
-let totalPoints = 29;
+let totalPoints = 10;
 // questions.length
 
-let wrongAnswers = 20;
-let result = 9;
 
 let varPercent1 = (result / totalPoints) * 100;
 
-let varPercent2 = (wrongAnswers / totalPoints) * 100;
+let varPercent2 = (10 - result / totalPoints) * 100;
 
 // segment.setattr = `${var1} ${var2}`;
 
 console.log(segment);
 segment.setAttribute("stroke-dasharray", `${varPercent1} ${varPercent2}`);
+
+// let wrongAnswers = 0;
+// let result = 10;
