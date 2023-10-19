134f40746993b7b33c35a7c7d51f55a76a9c66a1
Alexrocc <alerocc70@gmail.com>
2023-10-19 09:30:25 +0200

    small corrections

--- questions.js
+++ questions.js
@@ -8,8 +8,8 @@ const questions = [
     incorrect_answers: [
       "Central Process Unit",
       "Computer Personal Unit",
-      "Central Processor Unit"
-    ]
+      "Central Processor Unit",
+    ],
   },
   {
     category: "Science: Computers",
@@ -18,7 +18,7 @@ const questions = [
     question:
       "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
     correct_answer: "Final",
-    incorrect_answers: ["Static", "Private", "Public"]
+    incorrect_answers: ["Static", "Private", "Public"],
   },
   {
     category: "Science: Computers",
@@ -26,7 +26,7 @@ const questions = [
     difficulty: "easy",
     question: "The logo for Snapchat is a Bell.",
     correct_answer: "False",
-    incorrect_answers: ["True"]
+    incorrect_answers: ["True"],
   },
   {
     category: "Science: Computers",
@@ -35,7 +35,7 @@ const questions = [
     question:
       "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
-    incorrect_answers: ["True"]
+    incorrect_answers: ["True"],
   },
   {
     category: "Science: Computers",
@@ -44,7 +44,7 @@ const questions = [
     question:
       "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
-    incorrect_answers: [".png", ".jpeg", ".gif"]
+    incorrect_answers: [".png", ".jpeg", ".gif"],
   },
   {
     category: "Science: Computers",
@@ -55,8 +55,8 @@ const questions = [
     incorrect_answers: [
       "Counter Strike: Source",
       "Corrective Style Sheet",
-      "Computer Style Sheet"
-    ]
+      "Computer Style Sheet",
+    ],
   },
   {
     category: "Science: Computers",
@@ -65,7 +65,7 @@ const questions = [
     question:
       "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
-    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"]
+    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
   },
   {
     category: "Science: Computers",
@@ -73,7 +73,7 @@ const questions = [
     difficulty: "easy",
     question: "On Twitter, what is the character limit for a Tweet?",
     correct_answer: "140",
-    incorrect_answers: ["120", "160", "100"]
+    incorrect_answers: ["120", "160", "100"],
   },
   {
     category: "Science: Computers",
@@ -81,7 +81,7 @@ const questions = [
     difficulty: "easy",
     question: "Linux was first created as an alternative to Windows XP.",
     correct_answer: "False",
-    incorrect_answers: ["True"]
+    incorrect_answers: ["True"],
   },
   {
     category: "Science: Computers",
@@ -90,8 +90,8 @@ const questions = [
     question:
       "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
-    incorrect_answers: ["Python", "C", "Jakarta"]
-  }
+    incorrect_answers: ["Python", "C", "Jakarta"],
+  },
 ];
 
 window.onload = function () {
@@ -236,6 +236,7 @@ const generateResult = function () {
   document.getElementById("result-text-div").style.display = "block";
 
   if (result < 6) {
+    //Si rende dinamico il testo a seconda del risultato del test
     document.getElementById(
       "result-text-div"
     ).innerHTML = `<h3 id='result-text-title'>
@@ -248,33 +249,9 @@ const generateResult = function () {
   </p>`;
   }
 
-  const container = document.getElementById("container");
-
-  const segment = document.getElementsByClassName("ring")[0];
-
+  const segment = document.getElementsByClassName("ring")[0]; //Qui la svg viene modellata in base al risultato
   let totalPoints = questions.length;
-
   let varPercent2 = (result / totalPoints) * 100;
-
   let varPercent1 = ((totalPoints - result) / totalPoints) * 100;
-
-  console.log(segment);
   segment.setAttribute("stroke-dasharray", `${varPercent1} ${varPercent2}`);
 };
-
-// let ctx = document.getElementById("myDoughnutChart").getContext("2d");
-
-// let data = {
-//   datasets: [
-//     {
-//       data: [result, 10 - result],
-//       backgroundColor: ["#00FFFF", "#D20094"],
-//     },
-//   ],
-// };
-
-// let myDoughnutChart = new Chart(ctx, {
-//   type: "doughnut",
-//   data: data,
-//   options: { cutout: "70%", borderWidth: 0 }
-// });
